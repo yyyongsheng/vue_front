9c40e749b6e6c1d6a4079b91256987cf72410e2d
liuyongsheng <925931248@qq.com>
2017-12-20 11:25:03 +0800

    初始化

---
+++ src/component/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+	test
+	</div>
+    
+</template>
+
+<script>
+    export default {
+ 
+       
+        
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/component/admin/Admin.vue
@@ -0,0 +1,64 @@
+<template>
+<el-container>
+  <!-- 左侧公共部分 -->
+  <el-aside width="200px">
+    <app-aside> </app-aside>
+  </el-aside>
+
+  <el-container>
+    <!-- 头部公共部分 -->
+    <el-header>
+       <app-header></app-header> 
+     </el-header>
+     <!-- 动态变化部分 -->
+    <el-main>
+      <router-view>
+       
+      </router-view>
+    </el-main>
+  </el-container>
+</el-container>
+</template>
+
+<script>
+ import AsideComponent from "./frame/Aside.vue";
+ import HeaderComponent from "./frame/Header.vue";
+  export default {
+     components:{
+       appAside:AsideComponent,
+       appHeader:HeaderComponent,
+     }
+
+  }
+</script>
+
+<style scoped lang="less">
+.el-container{
+  height: 100%;
+}
+  
+  .el-aside {
+    background-color: orangered;
+    color: white;
+    text-align: center;
+    border-right: 3px solid yellow;
+   
+  }
+  .el-header{
+    // padding: 0;
+    background: rgba(243, 5, 5, 0.932);
+    color: white;
+    text-align: right;
+    line-height: 60px;
+    
+  }
+  .el-main {
+    background-color:orange;
+    color: white;
+   
+   // text-align: center;
+  }
+ 
+  
+
+</style>
